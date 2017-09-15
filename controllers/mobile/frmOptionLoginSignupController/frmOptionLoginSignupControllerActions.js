5367b085960766ea1645be63eb7635f23e4b0f7b
grahamdesmon <graham.desmon.simanjuntak@gmail.com>
2017-09-16 03:02:05 +0700

    add signup logic

--- controllers/mobile/frmOptionLoginSignupController/frmOptionLoginSignupControllerActions.js
+++ controllers/mobile/frmOptionLoginSignupController/frmOptionLoginSignupControllerActions.js
@@ -2,6 +2,16 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    AS_Button_gc7a422c4be8469bb7f4f34d04cb6ff5: function AS_Button_gc7a422c4be8469bb7f4f34d04cb6ff5(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmLogin");
+        ntf.navigate();
+    },
+    AS_Button_d37bcefe73d74e108aa22fb52f46c1b7: function AS_Button_d37bcefe73d74e108aa22fb52f46c1b7(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmSignUp");
+        ntf.navigate();
+    },
     AS_Form_a3eb455aab42470caeb25670efea35ee: function AS_Form_a3eb455aab42470caeb25670efea35ee(eventobject) {
         var self = this;
 
@@ -22,15 +32,5 @@ define({
         }, {
             "animationEnd": MOVE_ACTION____cd770cd82724494e8a9b92b1aabefdf6_Callback
         });
-    },
-    AS_Button_gc7a422c4be8469bb7f4f34d04cb6ff5: function AS_Button_gc7a422c4be8469bb7f4f34d04cb6ff5(eventobject) {
-        var self = this;
-        var ntf = new kony.mvc.Navigation("frmLogin");
-        ntf.navigate();
-    },
-    AS_Button_d37bcefe73d74e108aa22fb52f46c1b7: function AS_Button_d37bcefe73d74e108aa22fb52f46c1b7(eventobject) {
-        var self = this;
-        var ntf = new kony.mvc.Navigation("frmSignUp");
-        ntf.navigate();
     }
 });
